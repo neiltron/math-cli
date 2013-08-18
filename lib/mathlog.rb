6e86bdc493f015a2c454c2f1082225069cb4cdae
Neil Pullman <neil@descend.org>
2013-08-18 19:06:43 -0400

    abstract api code to a separate gem

--- lib/mathlog.rb
+++ lib/mathlog.rb
@@ -1,5 +1,5 @@
 require 'mathlog/version.rb'
-require 'mathlog/api.rb'
+require 'math-api'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
--- lib/mathlog/commands/import.rb
+++ lib/mathlog/commands/import.rb
@@ -1,11 +1,13 @@
 require 'CSV'
-require 'pp'
+
+@api = Math::API.new( accesskey: ENV['MATH_ACCESSKEY'], user_id: ENV['MATH_USERID'], math_url: ENV['MATH_URL'] || 'https://mathematics.io' )
 
 desc "Import Fitbit-exported CSVs"
 command [:import] do |c|
 
   c.desc "Load records from Fitbit CSV"
   c.arg_name 'filename'
+
   c.command :fitbit do |fitbit|
     fitbit.action do |global,options,args|
 
@@ -13,9 +15,9 @@ command [:import] do |c|
 
         next if i <= 1
 
-        Mathlog::API.submit({  timestamp:  row[0],  item_name:   'weight',  amount:  row[1]  })
-        Mathlog::API.submit({  timestamp:  row[0],  item_name:      'bmi',  amount:  row[2]  })
-        Mathlog::API.submit({  timestamp:  row[0],  item_name:  'bodyfat',  amount:  row[3]  })
+        @api.create_records({  timestamp:  row[0],  item_name:   'weight',  amount:  row[1]  })
+        @api.create_records({  timestamp:  row[0],  item_name:      'bmi',  amount:  row[2]  })
+        @api.create_records({  timestamp:  row[0],  item_name:  'bodyfat',  amount:  row[3]  })
 
       end
 
